b9ae948ea8f046338cca586702971d9cdfcb2107
Alphaeu <dealphaeus@gmail.com>
2023-04-11 23:22:26 -0700

    My static libraries tasks

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+ /**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+    return strcat(dest, src);
+}
