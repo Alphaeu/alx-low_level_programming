53eb1f405f7ca3d8b05c39b11bec25874d49f667
Alphaeu <dealphaeus@gmail.com>
2023-10-17 14:15:49 -0700

    Completed tasks

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-    printf("%d\n", _strlen("My Dyn Lib"));
-    return (EXIT_SUCCESS);
+	printf("%d\n", _strlen("Holberton"));
+	return (EXIT_SUCCESS);
 }
